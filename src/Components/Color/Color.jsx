259b8ab7c46efe6646c031bc1445e8d1d2cc22f6
Yeon Ji Lee <yonzelle@naver.com>
2026-04-22 18:50:38 +0900

    1st-task-done

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,5 +1,15 @@
 import "./Color.css";
 
 export default function Color({ color }) {
-  return null;
+  return (
+    <li id={color.id} className="color-card" style={{ background: color.hex }}>
+      <h2 className="color-card-headline">{color.hex}</h2>
+      <p className="color-card-role" style={{ color: color.contrastText }}>
+        {color.role}
+      </p>
+      <p className="color-card-contrast" style={{ color: color.contrastText }}>
+        {color.contrastText}
+      </p>
+    </li>
+  );
 }
