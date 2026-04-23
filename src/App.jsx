a204a2e5199cea0cf4c2a9447962e7fe0b93bba5
Yeon Ji Lee <yonzelle@naver.com>
2026-04-23 18:53:28 +0900

    new-delete-color

--- src/App.jsx
+++ src/App.jsx
@@ -15,25 +15,38 @@ function App() {
   function handleDeleteColor(idToDelete) {
     setColors(colors.filter((color) => color.id !== idToDelete));
   }
+
+  //     function handleEditColor(idToEdit) {
+
+  // const [edit, setEdit] = useState("");
+
+  //   }
   return (
     <>
       <h1>Theme Creator</h1>
+
       <ColorForm className="color-form" onSubmitColor={handleAddColor} />
       <div className="colorTheme">
-        <ul>
-          {colors.map((color) => (
-            <li className="color-form-list" key={color}>
-              <Color
-                id={color.id}
-                color={color}
-                //hex={color.hex}
-                role={color.role}
-                contrastText={color.contrastText}
-                onDeleteColor={handleDeleteColor}
-              />
-            </li>
-          ))}
-        </ul>
+        {colors.length === 0 ? (
+          <p className="color-card-highlight">
+            No colors in your theme yet... Start by adding one!!
+          </p>
+        ) : (
+          <ul>
+            {colors.map((color) => (
+              <li className="color-form-list" key={color.id}>
+                <Color
+                  id={color.id}
+                  color={color}
+                  //hex={color.hex}
+                  role={color.role}
+                  contrastText={color.contrastText}
+                  onDeleteColor={handleDeleteColor}
+                />
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </>
   );
